b7b5198c031f23c9e035657db44c1581b3987f92
Joy Lee <purple402@naver.com>
2022-07-14 16:04:48 +0900

    Create Routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Main, Search, Statistics, Write } from './screens';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/statistics" element={<Statistics />} />
+      </Routes>
     </div>
   );
 }
