6f5129f546470990a766c47c7686ec204c27c806
Joy Lee <purple402@naver.com>
2022-07-14 20:17:13 +0900

    Create index.tsx in /components

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import BottomNav from './components/BottomNav';
+import { BottomNav } from './components';
 
 function AppRouter() {
   return (
