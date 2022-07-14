b09216369c96884a5299f4580e0d99c977cd7da7
Joy Lee <purple402@naver.com>
2022-07-14 20:23:01 +0900

    Create CountDiary in Main.tsx

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Title } from '../components';
+import styled from 'styled-components';
+const CountDiary = styled.div`
+  font-size: 15px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 40px;
+`;
 
 function Main() {
   return (
     <div>
       <Title />
+      <CountDiary>
+        <span>00개의 일기 목록</span>
+      </CountDiary>
     </div>
   );
 }
