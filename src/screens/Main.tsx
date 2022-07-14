5296ac13f1f360e9df8bb08586cbb330bf0dd613
Joy Lee <purple402@naver.com>
2022-07-14 20:17:59 +0900

    Remove useless codes in Main.tsx

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Main() {
   return (
     <div>
-      <h1>Main!</h1>
-      <Link to="/write">작성하기</Link>
     </div>
   );
 }
