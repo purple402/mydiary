75ef8e2dba4a1b6431ea2237dc1ac5b63bc5265d
Joy Lee <purple402@naver.com>
2022-07-15 13:21:26 +0900

    Create onChange() in Search.tsx

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Title } from '../components';
+
+interface SearchInputsType {
+  startDate: string;
+  endDate: string;
+  tags: string[];
+}
 
 const SearchDiv = styled.div`
   padding: 0 20px;
@@ -10,6 +16,7 @@ const StyledLabel = styled.label`
   display: flex;
   margin: 6px 0;
   align-items: center;
+  justify-content: center;
 `;
 
 const StyledDateInput = styled.input`
@@ -25,18 +32,42 @@ const StyledInput = styled.input`
   border: 1px solid rgba(128, 128, 128, 0.253);
 `;
 
+const ButtonDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledButton = styled.button`
+  margin-bottom: 5px;
+`;
+
 function Search() {
+  // 입력값 관리
+  const [inputs, setInputs] = useState<SearchInputsType>({
+    startDate: '',
+    endDate: '',
+    tags: [],
+  });
+  const { startDate, endDate, tags }: SearchInputsType = inputs;
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value, id } = e.target;
+    setInputs({
+      ...inputs,
+      [id]: value,
+    });
+  }
+
   return (
     <div>
       <Title />
       <form>
         <SearchDiv>
           <StyledLabel htmlFor="startDate">
-            <StyledDateInput id="startDate" type="date" />
+            <StyledDateInput id="startDate" type="date" onChange={(e) => onChange(e)} />
             부터
           </StyledLabel>
           <StyledLabel htmlFor="endDate">
-            <StyledDateInput id="endDate" type="date" />
+            <StyledDateInput id="endDate" type="date" onChange={(e) => onChange(e)} />
             까지
           </StyledLabel>
           <StyledLabel htmlFor="tag">
